f81769af4060d49352e74db72fbeda8f0b7182e9
Vinz <vinzkarloemendoza@gmail.com>
2022-03-23 17:18:31 +0800

    Changed profile image update button to not update automatically after file select

--- pages/profile.js
+++ pages/profile.js
@@ -1,142 +1,3 @@
-// import { useCallback, useEffect, useState } from "react";
-// import Auth from "../components/Auth";
-// import PageLayout from "../components/Layout/PageLayout";
-
-// import { useAuth } from "../utils/context/Auth";
-// import { supabase } from "../utils/supabaseClient";
-// import Avatar from "../components/Avatar";
-
-// const Profile = () => {
-//   const { user } = useAuth();
-
-//   const [loading, setLoading] = useState(true);
-//   const [profile, setProfile] = useState([]);
-
-//   useEffect(() => {
-//     fetchProfile();
-//   }, [fetchProfile]);
-
-//   const fetchProfile = useCallback(async () => {
-//     try {
-//       const { data, error } = await supabase
-//         .from("profiles")
-//         .select("id, email, avatar_url")
-//         .eq("id", user?.id)
-//         .single();
-
-//       if (error) console.log(error);
-
-//       if (!data) return;
-
-//       setProfile(data);
-//     } catch (err) {
-//       console.log(err);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }, [user?.id]);
-
-//   async function updateProfile(avatar_url) {
-//     try {
-//       setLoading(true);
-
-//       const updates = {
-//         id: user.id,
-//         avatar_url,
-//         updated_at: new Date(),
-//       };
-
-//       let { error } = await supabase.from("profiles").upsert(updates, {
-//         returning: "minimal",
-//       });
-
-//       if (error) {
-//         throw error;
-//       }
-//     } catch (error) {
-//       alert(error.message);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
-
-//   async function uploadAvatar(event) {
-//     try {
-//       setLoading(true);
-
-//       if (!event.target.files || event.target.files.length === 0) {
-//         throw new Error("You must select an image to upload.");
-//       }
-
-//       const file = event.target.files[0];
-//       const fileExt = file.name.split(".").pop();
-//       const fileName = `${Math.random()}.${fileExt}`;
-//       const filePath = `${fileName}`;
-
-//       let { error: uploadError } = await supabase.storage
-//         .from("avatars")
-//         .upload(filePath, file);
-
-//       if (uploadError) {
-//         throw uploadError;
-//       }
-
-//       updateProfile(filePath);
-//     } catch (error) {
-//       alert(error.message);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
-
-//   if (!user) return <Auth />;
-
-//   if (loading)
-//     return (
-//       <PageLayout title="loading">
-//         <div className={`relative w-16 h-16 overflow-hidden`}>
-//           <div className="w-16 h-16 bg-gray-600 rounded-full"></div>
-//         </div>
-//       </PageLayout>
-//     );
-
-//   return (
-//     <PageLayout title="Profile">
-//       {/* <Avatar url={profile.avatar_url} size={48} /> */}
-//       <div className={`relative w-16 h-16 overflow-hidden`}>
-//         {profile.avatar_url !== null ? (
-//           <Avatar url={profile.avatar_url} size={16} />
-//         ) : (
-//           <div
-//             className={`relative flex items-center justify-center w-16 h-16 overflow-hidden bg-zinc-300 dark:bg-zinc-400 rounded-full `}
-//           >
-//             <span className="text-5xl">{user.email.charAt(0)}</span>
-//           </div>
-//         )}
-//         {/* <Avatar url={profile.avatar_url} size={16} /> */}
-//       </div>
-//       <div>
-//         <label className="block p-2 bg-blue-400 rounded" htmlFor="single">
-//           {loading ? "Uploading ..." : "Upload"}
-//         </label>
-//         <input
-//           style={{
-//             visibility: "hidden",
-//             position: "absolute",
-//           }}
-//           type="file"
-//           id="single"
-//           accept="image/*"
-//           onChange={uploadAvatar}
-//           disabled={loading}
-//         />
-//       </div>
-//     </PageLayout>
-//   );
-// };
-
-// export default Profile;
-
 import { useCallback, useEffect, useState } from "react";
 import Auth from "../components/Auth";
 import PageLayout from "../components/Layout/PageLayout";
@@ -146,13 +7,17 @@ import { useAuth } from "../utils/context/Auth";
 import { supabase } from "../utils/supabaseClient";
 import Avatar from "../components/Avatar";
 import { useAvatar } from "../utils/context/Avatar";
+import { toast } from "react-toastify";
+import { useTheme } from "next-themes";
 
 const Profile = () => {
   const { user } = useAuth();
   const { profile } = useAvatar();
   const router = useRouter();
+  const { resolvedTheme } = useTheme();
 
   const [loading, setLoading] = useState(false);
+  const [profileImage, setProfileImage] = useState({});
 
   async function updateProfile(avatar_url) {
     try {
@@ -179,15 +44,18 @@ const Profile = () => {
     }
   }
 
-  async function uploadAvatar(event) {
+  async function uploadAvatar() {
     try {
       setLoading(true);
 
-      if (!event.target.files || event.target.files.length === 0) {
+      if (
+        !profileImage.target.files ||
+        profileImage.target.files.length === 0
+      ) {
         throw new Error("You must select an image to upload.");
       }
 
-      const file = event.target.files[0];
+      const file = profileImage.target.files[0];
       const fileExt = file.name.split(".").pop();
       const fileName = `${Math.random()}.${fileExt}`;
       const filePath = `${fileName}`;
@@ -201,12 +69,25 @@ const Profile = () => {
       }
 
       updateProfile(filePath);
+      setLoading(false);
     } catch (error) {
-      alert(error.message);
-    } finally {
+      toast.error("Failed to upload image, please try again.", {
+        theme: resolvedTheme,
+      });
       setLoading(false);
     }
   }
+
+  const getFile = async (file) => {
+    try {
+      setLoading(true);
+      setProfileImage(file);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      throw new Error(err);
+    }
+  };
 
   const signOut = async () => {
     try {
@@ -226,7 +107,7 @@ const Profile = () => {
       <PageLayout title="loading">
         <div className="flex flex-col items-center justify-center">
           <div className={`relative w-32 h-32 overflow-hidden`}>
-            <div className="w-32 h-32 bg-gray-600 rounded-full"></div>
+            <div className="w-32 h-32 rounded-full bg-zinc-600"></div>
           </div>
         </div>
       </PageLayout>
@@ -247,39 +128,40 @@ const Profile = () => {
           )}
         </div>
 
-        <div className="mb-8">
-          <label
-            className="block p-2 text-sm rounded cursor-pointer dark:text-gray-300 hover:dark:text-gray-50 hover:bg-blue-400"
-            htmlFor="single"
-          >
-            {loading ? "Uploading ..." : "Change Profile Picture"}
-          </label>
+        <div className="flex flex-col items-center justify-center mb-24 space-y-4">
           <input
-            style={{
-              visibility: "hidden",
-              position: "absolute",
-            }}
             type="file"
-            id="single"
             accept="image/*"
-            onChange={uploadAvatar}
+            onChange={getFile}
             disabled={loading}
+            className="block w-full text-sm text-zinc-500 file:bg-white file:text-blue-600 hover:file:bg-blue-200 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold dark:file:bg-blue-50 dark:file:text-blue-600 dark:hover:file:bg-blue-100 "
           />
+          <button
+            type="submit"
+            onClick={uploadAvatar}
+            disabled={loading || Object.keys(profileImage).length < 1}
+            className="px-6 py-2 bg-blue-500 rounded-md text-zinc-100 disabled:cursor-not-allowed dark:disabled:bg-zinc-700 dark:disabled:hover:bg-zinc-600 disabled:bg-zinc-200 disabled:hover:bg-zinc-300 disabled:text-zinc-900 dark:disabled:text-zinc-100"
+          >
+            Update Profile Image
+          </button>
         </div>
         <div className="flex flex-col gap-y-4">
-          <label htmlFor="email" className="text-sm text-gray-300">
+          <label
+            htmlFor="email"
+            className="text-sm text-zinc-700 dark:text-zinc-300"
+          >
             Email
           </label>
           <input
             name="email"
-            className="p-2 text-center bg-gray-800 rounded-md hover:cursor-not-allowed"
+            className="p-2 text-center rounded-md bg-zinc-300 dark:bg-zinc-800 hover:cursor-not-allowed"
             defaultValue={profile.email}
             disabled
           />
           <button
             onClick={signOut}
             disabled={loading}
-            className="px-2 py-1 mt-4 bg-blue-400 rounded-md hover:bg-blue-600"
+            className="px-2 py-1 mt-4 bg-red-500 rounded-md hover:bg-red-600 text-zinc-100"
           >
             Sign out
           </button>
